fbd5c0e161cb4d65bd4dea0770bcf31a8bb8bd0c
ElectroCubic <anushbundel26@gmail.com>
2026-02-03 11:28:54 +0530

    Added icons in hamburger menu

--- src/assets/Button/Button.jsx
+++ src/assets/Button/Button.jsx
@@ -1,20 +1,4 @@
 
-// import PropTypes from "prop-types"
-// import styles from "./Button.module.css"
-
-// function Button({text="Click Me", className="", ...props }) {
-//   return (
-//     <button className={`${styles.button} ${className}`}{...props}> {text} </button>
-//   );
-// }
-
-// Button.propTypes = {
-//     text: PropTypes.string,
-//     className: PropTypes.string,
-// };
-
-// export default Button
-
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
